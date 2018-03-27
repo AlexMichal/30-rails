c28c5d26b31ad4b76915bfb8a8924a78101a0999
AlexMichal <alex.michal@hotmail.com>
2018-03-26 19:01:37 -0700

    Added node to project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: '30 Rails' 
+  });
 });
 
 module.exports = router;
